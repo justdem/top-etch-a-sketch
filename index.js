79e66b4b0e8a8863b3528b5b77c74b17f258f038
justdem <8630623+justdem@users.noreply.github.com>
2023-04-28 19:13:36 -0700

    Create 16 rows

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const containerDiv = document.querySelector('.container')
 
-const div = document.createElement('div')
-div.classList.add('row');
-containerDiv.appendChild(div);
-
 for (let i=1; i<=16; i++) {
-    const divCell = document.createElement('div')
-    divCell.classList.add('cell');
-    div.appendChild(divCell);
-}
+    const div = document.createElement('div')
+    div.classList.add('row');
+    containerDiv.appendChild(div);
 
+    for (let i=1; i<=16; i++) {
+        const divCell = document.createElement('div')
+        divCell.classList.add('cell');
+        div.appendChild(divCell);
+    }
+}
 //create div that will act as row
 //create 16 divs within that div to act as cells
 //loop 16 times to add div to container div
