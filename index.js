5f75d6954dd56010bd2d18ddfe64e0ddd8d57192
justdem <8630623+justdem@users.noreply.github.com>
2023-04-28 21:16:50 -0700

    Reset grid with new dimensions

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const containerDiv = document.querySelector('.container')
+const resetButton = document.querySelector('button.reset');
+resetButton.addEventListener('click', resetGrid);
+const containerDiv = document.querySelector('.container');
+newGrid(16);
 
-for (let i=1; i<=16; i++) {
-    const div = document.createElement('div')
-    div.classList.add('row');
-    containerDiv.appendChild(div);
+function newGrid(dimension) {
+    for (let i=1; i<=dimension; i++) {
+        const div = document.createElement('div')
+        div.classList.add('row');
+        containerDiv.appendChild(div);
 
-    for (let i=1; i<=16; i++) {
-        const divCell = document.createElement('div')
-        divCell.classList.add('cell');
-        div.appendChild(divCell);
+        for (let i=1; i<=dimension; i++) {
+            const divCell = document.createElement('div')
+            divCell.classList.add('cell');
+            div.appendChild(divCell);
+        }
     }
-}
 
-const divCells = document.querySelectorAll('.cell')
-for (let i=0; i<divCells.length; i++) {
-    divCells[i].addEventListener('mouseover', (e)=> changeColor(e))
-};
+    const divCells = document.querySelectorAll('.cell');
+    for (let i=0; i<divCells.length; i++) {
+        divCells[i].addEventListener('mouseover', (e)=> changeColor(e))
+    };
+}
 
 function changeColor(e) {
     e.target.style.background = 'black';
 }
 
-const resetButton = document.querySelector('button.reset')
-resetButton.addEventListener('click', resetGrid);
-
 function resetGrid() {
-   const gridDimension = Number(prompt("Provide grid dimensions", 16));
-
-   const oldGrid = document.querySelectorAll('.row');
+   const gridDimension = prompt("Provide grid dimensions", 16);
+   if (gridDimension === null) return;
+   else {
+    const oldGrid = document.querySelectorAll('.row');
+    for (let i=0; i<oldGrid.length; i++) {oldGrid[i].remove()};
+    newGrid(Number(gridDimension));
+    }  
 }
